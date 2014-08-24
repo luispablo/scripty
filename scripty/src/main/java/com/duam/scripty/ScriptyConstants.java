82d05ac3b04d28d4648eed087b689f4c935a77c9
Luis Pablo <luispablo.gallo@gmail.com>
2014-08-24 19:27:04 -0300

    Poner la dirección del server en la web

--- scripty/src/main/java/com/duam/scripty/ScriptyConstants.java
+++ scripty/src/main/java/com/duam/scripty/ScriptyConstants.java
@@ -4,7 +4,7 @@ package com.duam.scripty;
  * Created by luispablo on 11/05/14.
  */
 public class ScriptyConstants {
-    public static final String SCRIPTY_SERVER_URL = "http://192.168.1.20:3000";
+    public static final String SCRIPTY_SERVER_URL = "http://scripty.duamsistemas.com.ar";
     public static final String FIND_USER_URI = "/users/find.json";
     public static final String USER_URI = "/users.json";
     public static final String CREATE_DEVICE_URI = "/users/:id/devices.json";
